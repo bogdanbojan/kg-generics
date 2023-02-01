2be75a03d1132f7f5484e520629e5bba04c0448c
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-02-01 13:41:56 +0200

    Solve exercise 4.1

--- exercises/4.1/go.mod
+++ exercises/4.1/go.mod
@@ -2,6 +2,7 @@ module product
 
 go 1.18
 
-require github.com/google/go-cmp v0.5.7
-
-require golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+require (
+	github.com/google/go-cmp v0.5.8
+	golang.org/x/exp v0.0.0-20230131160201-f062dba9d201
+)
--- exercises/4.1/product.go
+++ exercises/4.1/product.go
@@ -1,5 +1,13 @@
 package product
 
+import "golang.org/x/exp/constraints"
+
 // Your constraint definition goes here!
+type Number interface {
+	constraints.Integer | constraints.Float | constraints.Complex
+}
 
 // Your Product function goes here!
+func Product[T Number](x, y T) T {
+	return x * y
+}
