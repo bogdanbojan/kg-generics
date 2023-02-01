20f1ec563cd5131c4870a88e52b8357e078ebdd6
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-02-02 00:59:03 +0200

    Solve exercise 4.2

--- exercises/4.2/dupes.go
+++ exercises/4.2/dupes.go
@@ -1,3 +1,19 @@
 package dupes
 
 // Your Dupes function goes here!
+func Dupes[T comparable](s []T) bool {
+	seen := map[T]bool{}
+
+	if len(s) < 2 {
+		return false
+	}
+
+	for _, x := range s {
+		if seen[x] {
+			return true
+		}
+		seen[x] = true
+	}
+
+	return false
+}
