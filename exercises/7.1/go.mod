7ae6cd5b7a3f6033feb47ed80450f165219f68c7
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-02-16 17:23:49 +0200

    Solve exercise 7.1

--- exercises/7.1/go.mod
+++ exercises/7.1/go.mod
@@ -2,4 +2,7 @@ module stack
 
 go 1.18
 
-require github.com/google/go-cmp v0.5.7
+require (
+	github.com/google/go-cmp v0.5.8
+	golang.org/x/exp v0.0.0-20230213192124-5e25df0256eb
+)
--- exercises/7.1/stack.go
+++ exercises/7.1/stack.go
@@ -1,3 +1,24 @@
 package stack
 
+import "golang.org/x/exp/constraints"
+
 // Your implementation of Stack goes here!
+
+type Stack[T constraints.Ordered] []T
+
+func (s *Stack[T]) Push(vv ...T) {
+	*s = append(*s, vv...)
+}
+
+func (s Stack[T]) Len() int {
+	return len(s)
+}
+
+func (s *Stack[T]) Pop() (v T, ok bool) {
+	if len(*s) == 0 {
+		return v, false
+	}
+	v = (*s)[s.Len()-1]
+	*s = (*s)[:s.Len()-1]
+	return v, true
+}
