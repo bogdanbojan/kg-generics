001b5544ea8277b6987f022d49c8236713576131
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-02-17 10:05:35 +0200

    Solve exercise 6.1

--- exercises/6.1/funcmap.go
+++ exercises/6.1/funcmap.go
@@ -1,3 +1,8 @@
 package funcmap
 
 // Your implementation of FuncMap goes here!
+type FuncMap[T, U any] map[string]func(T) U
+
+func (f FuncMap[T, U]) Apply(operation string, thing T) U {
+	return f[operation](thing)
+}
