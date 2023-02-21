f6d2c6be26ac8346b9f60cf00d8912c3b11da9ae
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-02-21 10:59:30 +0200

    Solve exercise 9.2

--- exercises/9.2/merge.go
+++ exercises/9.2/merge.go
@@ -3,3 +3,11 @@ package merge
 import "golang.org/x/exp/maps"
 
 // Your implementation of Merge goes here!
+func Merge[K comparable, V any](mm ...map[K]V) map[K]V {
+	res := map[K]V{}
+
+	for _, m := range mm {
+		maps.Copy(res, m)
+	}
+	return res
+}
