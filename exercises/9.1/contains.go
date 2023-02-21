9900152483b58fcfbbeac1185cb77d1ea93f8281
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-02-21 10:42:57 +0200

    Solve exercise 9.1

--- exercises/9.1/contains.go
+++ exercises/9.1/contains.go
@@ -3,3 +3,6 @@ package contains
 import "golang.org/x/exp/slices"
 
 // Your implementation of ContainsFunc goes here!
+func ContainsFunc[T any](s []T, f func(T) bool) bool {
+	return slices.IndexFunc(s, f) >= 0
+}
